d891241c385ce35d0113d28192613c0860df33bc
Jason Christopher <jason.christopher@u.nus.edu>
2023-11-04 11:45:02 +0800

    Complete React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+import GossipBox from "./components/games/GossipBox.jsx";
+import RandomCatFact from "./components/games/RandomCatFact.jsx";
+
+// import statements
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/gossipbox",
+    element: <GossipBox />,
+  },
+  {
+    path: "/catfact",
+    element: <RandomCatFact />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
