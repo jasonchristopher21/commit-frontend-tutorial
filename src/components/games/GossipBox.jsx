fd432f98fa09afc6d84604e16872a8e904cff157
Jason Christopher <jason.christopher@u.nus.edu>
2023-11-04 12:22:12 +0800

    Complete Axios

--- src/components/games/GossipBox.jsx
+++ src/components/games/GossipBox.jsx
@@ -1,35 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_URL } from "../../constants";
 export default function GossipBox() {
   const [gossip, setGossip] = useState("");
   const [gossipList, setGossipList] = useState([]);
 
+  function fetchGossips() {
+    axios.get(API_URL + "post").then((response) => {
+      setGossipList(response.data);
+    });
+  }
+  useEffect(() => {
+    fetchGossips();
+  }, []);
+
   function addGossip() {
-    setGossipList([...gossipList, gossip]);
-    setGossip("");
+    axios
+    .post(API_URL + "post", { body: gossip })
+    .then((response) => {
+      fetchGossips();
+    })
   }
 
   return (
-    <div className="container mx-auto flex flex-col gap-2 text-center items-center">
-      <h1>Gossip box</h1>
+    <div className="container mx-auto flex flex-col gap-4 text-center items-center my-10">
+      <h1 className="font-bold text-2xl">Gossip Box</h1>
       <input
         value={gossip}
         onChange={(e) => {
           setGossip(e.target.value);
         }}
-        className="border border-gray-400 py-2 px-4 w-1/2 rounded-full"
+        className="border border-gray-400 py-2 px-4 w-1/2 rounded-lg"
         placeholder="Enter your gossip here"
       />
       <div>
         <button
           onClick={addGossip}
-          className="bg-orange-400 py-1 px-5 rounded-full"
+          className="bg-orange-400 py-1 px-5 rounded-lg"
         >
           Gossip now and get your sin
         </button>
       </div>
       <div>
         {gossipList.map((gossip, index) => (
-          <div key={index}>{gossip}</div>
+          <div key={index}>{gossip.body}</div>
         ))}
       </div>
     </div>
--- src/components/games/RandomCatFact.jsx
+++ src/components/games/RandomCatFact.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../constants";
 
 export default function RandomCatFact() {
+  const [catFact, setCatFact] = useState(
+    "In the cartoon Tom & Jerry, Tom is a Cat :O :O :O"
+  );
 
-    const [catFact, setCatFact] = useState('In the cartoon Tom & Jerry, Tom is a Cat :O :O :O')
+  function getCatFact() {
+    axios
+      .get(API_URL + "catfact")
+      .then((response) => {
+        setCatFact(response.data.fact);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    // TODO: Make an API call here every time the user refreshes the page
-
-    return (
-        <div>
-            <h1>Random Cat Fact</h1>
-            <p>{catFact}</p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-4 my-10 mx-auto text-center max-w-xl px-10">
+      <h1 className="font-bold text-2xl">Random Cat Fact</h1>
+      <p>{catFact}</p>
+      <div className="flex mx-auto">
+        <button className="text-white bg-blue-500 py-1 px-5 rounded-lg hover:bg-blue-300" onClick={getCatFact}>
+          Get a random cat fact
+        </button>
+      </div>
+    </div>
+  );
 }
